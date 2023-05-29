0876f8db70ba4f9de1c537275547e4e7e879a2fd
Adam Fadilah Islamawan <adam.fadilah17@gmail.com>
2023-05-29 14:35:40 +0700

    login_page_xml

---
+++ app/src/main/java/com/example/profolio/login/LoginPageActivity.java
@@ -0,0 +1,42 @@
+package com.example.profolio.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ViewTreeObserver;
+import android.widget.LinearLayout;
+
+import com.example.profolio.R;
+
+public class LoginPageActivity extends AppCompatActivity {
+    private LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+        linearLayout = findViewById(R.id.loginPage);
+        linearLayout.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                linearLayout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                float startPosition = linearLayout.getY() + linearLayout.getHeight();
+                float endPosition = linearLayout.getY();
+                ValueAnimator animator = ValueAnimator.ofFloat(startPosition, endPosition);
+                animator.setDuration(750);
+                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        float animatedValue = (float) animation.getAnimatedValue();
+                        linearLayout.setY(animatedValue);
+                    }
+                });
+                animator.start();
+            }
+        });
+    }
+}
