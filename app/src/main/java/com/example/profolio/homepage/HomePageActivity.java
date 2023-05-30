fabaef4d633d1e0b98dd58bd8ebab8afdd1d142f
MuhAtthar <muhatthar@student.ub.ac.id>
2023-05-30 11:05:43 +0700

    membuat homepage

--- app/src/main/java/com/example/profolio/homepage/HomePageActivity.java
+++ app/src/main/java/com/example/profolio/homepage/HomePageActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.profolio.R;
+
 public class HomePageActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/example/profolio/login/LoginPageActivity.java
+++ app/src/main/java/com/example/profolio/login/LoginPageActivity.java
@@ -6,15 +6,18 @@ import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ViewTreeObserver;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.profolio.R;
+import com.example.profolio.homepage.HomePageActivity;
 import com.example.profolio.register.RegisterPageActivity;
 
 public class LoginPageActivity extends AppCompatActivity {
     private LinearLayout linearLayout;
     private TextView signUp;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,7 @@ public class LoginPageActivity extends AppCompatActivity {
 
         signUp = findViewById(R.id.signUp);
         linearLayout = findViewById(R.id.loginPage);
+        btnLogin = findViewById(R.id.btn_login);
         linearLayout.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
@@ -46,6 +50,11 @@ public class LoginPageActivity extends AppCompatActivity {
         signUp.setOnClickListener(v -> {
             Intent next = new Intent(this, RegisterPageActivity.class);
             startActivity(next);
+        });
+
+        btnLogin.setOnClickListener(v ->{
+            Intent login = new Intent(this, HomePageActivity.class);
+            startActivity(login);
         });
     }
 }
