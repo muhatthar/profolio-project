1b054ab84464f634b774feff0c0d224533471f13
Adam Fadilah Islamawan <adam.fadilah17@gmail.com>
2023-06-01 23:04:38 +0700

    terserah

--- app/src/main/java/com/example/profolio/homepage/HomePageActivity.java
+++ app/src/main/java/com/example/profolio/homepage/HomePageActivity.java
@@ -12,6 +12,7 @@ import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.profolio.R;
--- app/src/main/java/com/example/profolio/homepage/HomePageFragment.java
+++ app/src/main/java/com/example/profolio/homepage/HomePageFragment.java
@@ -2,13 +2,17 @@ package com.example.profolio.homepage;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.profolio.AdapterFragment.SectionPagerAdapter;
 import com.example.profolio.R;
+import com.google.android.material.tabs.TabLayout;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,10 +21,15 @@ import com.example.profolio.R;
  */
 public class HomePageFragment extends Fragment {
 
+    View view;
+    ViewPager viewPager;
+    TabLayout tabLayout;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -61,6 +70,45 @@ public class HomePageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_page, container, false);
+        view =  inflater.inflate(R.layout.fragment_home_page, container, false);
+
+        viewPager = view.findViewById(R.id.viewPager);
+        tabLayout = view.findViewById(R.id.tabLayout);
+
+        return  view;
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setUpViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    private void setUpViewPager(ViewPager viewPager) {
+        SectionPagerAdapter adapter = new SectionPagerAdapter(getChildFragmentManager());
+        adapter.addFragment(new OrganisasiFragment(), "Organisasi");
+        adapter.addFragment(new KepanitiaanFragment(), "Kepanitiaan");
+        adapter.addFragment(new PrestasiFragment(), "Prestasi");
+
+        viewPager.setAdapter(adapter);
     }
 }
