4cb27e7c9ac9b2e0f572042892f205a2b7b0c581
Adam Fadilah Islamawan <adam.fadilah17@gmail.com>
2023-06-01 16:52:03 +0700

    update_fragment

--- app/src/main/java/com/example/profolio/homepage/HomePageActivity.java
+++ app/src/main/java/com/example/profolio/homepage/HomePageActivity.java
@@ -1,6 +1,8 @@
 package com.example.profolio.homepage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.animation.ValueAnimator;
 import android.os.Bundle;
